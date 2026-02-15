4549e4e9c793cac50b20f276fe4e0514e2849985
manjunathdg13 <157711950+manjunathdg13@users.noreply.github.com>
2026-02-15 19:25:41 +0530

    Add files via upload

---
+++ question/src/main/java/com/question/Service/QuestionService.java
@@ -0,0 +1,89 @@
+package com.question.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+
+import com.question.DTO.QuestionWrapper;
+import com.question.DTO.Response;
+import com.question.Entity.QuestionEntity;
+import com.question.Repository.QuestionRepo;
+
+@Service
+public class QuestionService {
+	
+    @Autowired
+    QuestionRepo questionDao;
+
+    public ResponseEntity<List<QuestionEntity>> getAllQuestions() {
+        try {
+            return new ResponseEntity<>(questionDao.findAll(), HttpStatus.OK);
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+        return new ResponseEntity<>(new ArrayList<>(), HttpStatus.BAD_REQUEST);
+    }
+
+    public ResponseEntity<List<QuestionEntity>> getQuestionsByCategory(String category) {
+        try {
+            return new ResponseEntity<>(questionDao.findByCategory(category),HttpStatus.OK);
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+        return new ResponseEntity<>(new ArrayList<>(), HttpStatus.BAD_REQUEST);
+
+    }
+
+    public ResponseEntity<String> addQuestion(QuestionEntity question) {
+        questionDao.save(question);
+        return new ResponseEntity<>("success",HttpStatus.CREATED);
+    }
+
+    public ResponseEntity<List<Integer>> getQuestionsForQuiz(String categoryName, Integer numQuestions) {
+    	Pageable pageable = PageRequest.of(0, numQuestions);
+    	 
+        List<Integer> questions = questionDao.findRandomQuestionsByCategory(categoryName, pageable);
+        return new ResponseEntity<>(questions, HttpStatus.OK);
+    }
+
+    public ResponseEntity<List<QuestionWrapper>> getQuestionsFromId(List<Integer> questionIds) {
+        List<QuestionWrapper> wrappers = new ArrayList<>();
+        List<QuestionEntity> questions = new ArrayList<>();
+
+        for(Integer id : questionIds){
+            questions.add(questionDao.findById(id).get());
+        }
+
+        for(QuestionEntity question : questions){
+            QuestionWrapper wrapper = new QuestionWrapper();
+            wrapper.setId(question.getId());
+            wrapper.setQuestionTitle(question.getQuestionTitle());
+            wrapper.setOption1(question.getOption1());
+            wrapper.setOption2(question.getOption2());
+            wrapper.setOption3(question.getOption3());
+            wrapper.setOption4(question.getOption4());
+            wrappers.add(wrapper);
+        }
+
+        return new ResponseEntity<>(wrappers, HttpStatus.OK);
+    }
+
+    public ResponseEntity<Integer> getScore(List<Response> responses) {
+
+
+        int right = 0;
+
+        for(Response response : responses){
+        	QuestionEntity question = questionDao.findById(response.getId()).get();
+            if(response.getResponse().equals(question.getRightAnswer()))
+                right++;
+        }
+        return new ResponseEntity<>(right, HttpStatus.OK);
+    }
+}
